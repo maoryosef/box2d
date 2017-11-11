fc72241285eaa07d2aee6d2800cc462e66fb1a04
Maor Yosef <maory@wix.com>
2017-11-11 12:57:29 +0200

    Rearrange for more demos

--- webpack.config.js
+++ webpack.config.js
@@ -1,20 +1,23 @@
-'use strict'
+'use strict';
+
 const path = require('path');
 
 const webpackConfig = {
-    entry: './src/index.js',
-    output: {
-        libraryTarget: 'umd',
-        path: path.resolve('dist'),
-        filename: 'bundle.js'
-    },
-    plugins: [
-    ],
-    resolve: {
-        extensions: ['.js', '.jsx']
-    },
-    devtool: 'source-map'
-}
+	entry: {
+		basic_demo: './src/basicDemo.js'
+	},
+	output: {
+		libraryTarget: 'umd',
+		path: path.resolve('dist'),
+		filename: '[name].bundle.js'
+	},
+	plugins: [
+	],
+	resolve: {
+		extensions: ['.js', '.jsx']
+	},
+	devtool: 'source-map'
+};
 
-module.exports = webpackConfig
+module.exports = webpackConfig;
 
